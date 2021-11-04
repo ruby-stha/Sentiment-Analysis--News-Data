ceef25f918b6e6fd46f65b8d4a8d6d5eac350493
Ruby Shrestha <stha.rubee@gmail.com>
2021-11-05 00:15:27 +0545

    Optimization

--- edit_web_scraped_file.py
+++ edit_web_scraped_file.py
@@ -1,50 +1,16 @@
 # Updating scraped news data to remove unnecessary textual units
 
+def replace_all(text, arr):
+  for each in arr:
+    text=text.replace(each,"")
+  return text
+
+to_replace= ["None", "Click here for your comments", "Comment via Facebook", "Don't have facebook account? Use this form to comment ", "Published: 02-07-2016"]
 f = open('hellotxt.csv','r')
 filedata = f.read()
 f.close()
-newdata = filedata.replace("None","")
+newdata = replace_all(filedata, to_replace)
 f = open('hellotxt.csv','w')
 f.write(newdata)
 f.close()
 
-f = open('hellotxt.csv','r')
-filedata = f.read()
-f.close()
-newdata = filedata.replace("Click here for your comments","")
-f = open('hellotxt.csv','w')
-f.write(newdata)
-f.close()
-
-f = open('hellotxt.csv','r')
-filedata = f.read()
-f.close()
-newdata = filedata.replace("Comment via Facebook","")
-f = open('hellotxt.csv','w')
-f.write(newdata)
-f.close()
-
-
-f = open('hellotxt.csv','r')
-filedata = f.read()
-f.close()
-newdata = filedata.replace("Don't have facebook account? Use this form to comment ","")
-f = open('hellotxt.csv','w')
-f.write(newdata)
-f.close()
-
-f = open('hellotxt.csv','r')
-filedata = f.read()
-f.close()
-newdata = filedata.replace("Published: 02-07-2016","")
-f = open('hellotxt.csv','w')
-f.write(newdata)
-f.close()
-
-f = open('hellotxt.csv','r')
-filedata = f.read()
-f.close()
-newdata = filedata.replace(",","")
-f = open('hellotxt.csv','w')
-f.write(newdata)
-f.close()
